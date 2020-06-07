53448acda0de895efa520408226dc5eed46fe800
Holly Prothe <hollyprothe1@gmail.com>
2020-06-07 15:04:58 -0500

    correct syntax errors on schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,27 @@ CREATE DATABASE employeesDB;
 USE employeesDB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(45) NULL,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(45) NULL,
     salary DECIMAL(10.3) NULL,
     department_id INT NULL,
-    PRIMARY KEY (id)
+    INDEX dep_ind (depart_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(45) NULL,
     last_name VARCHAR(45) NULL,
     role_id INT NULL,
     manager_id INT NULL,
-    PRIMARY KEY (id)
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE,
 );
