6676bf4d857977b03a95ac64597abef562017670
Holly Prothe <hollyprothe1@gmail.com>
2020-06-07 14:27:03 -0500

    add connections code

--- db/seed.sql
+++ db/seed.sql
@@ -10,7 +10,7 @@ INSERT INTO department (name)
 VALUE ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Leed", 100000, 1);
+VALUES ("Sales Lead", 100000, 1);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lead Engineer", 150000, 2);
 INSERT INTO role (title, salary, department_id)
@@ -20,3 +20,5 @@ VALUES ("Accountant", 125000, 3);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Legal Team Lead", 250000, 4);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3);
