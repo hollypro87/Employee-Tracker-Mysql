66ea897507ac79cafbe443ef6a7fc1be9e1e141a
Holly Prothe <hollyprothe1@gmail.com>
2020-06-07 13:55:04 -0500

    add sql code to seed for department table

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE employeesDB;
+
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
