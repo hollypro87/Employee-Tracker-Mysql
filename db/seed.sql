2e15dc18b28baeeee7e82403e3209ad0ca7add0f
Holly Prothe <hollyprothe1@gmail.com>
2020-06-07 14:00:34 -0500

    add sql code to seed for roles table

--- db/seed.sql
+++ db/seed.sql
@@ -8,3 +8,15 @@ INSERT INTO department (name)
 VALUE ("Finance");
 INSERT INTO department (name)
 VALUE ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Leed", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
+
