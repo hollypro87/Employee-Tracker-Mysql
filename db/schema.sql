f6cf50ffa0b5a85ff2623ea0bdc800f0c52f8def
Holly Prothe <hollyprothe1@gmail.com>
2020-06-07 16:04:17 -0500

    troubleshoot mysql

--- db/schema.sql
+++ db/schema.sql
@@ -6,16 +6,16 @@ USE employeesDB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(45) NULL,
+    name VARCHAR(45) NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(45) NULL,
-    salary DECIMAL(10.3) NULL,
+    salary DECIMAL(10) NULL,
     department_id INT NULL,
-    INDEX dep_ind (depart_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+	INDEX dep_ind (department_id),
+	CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -23,9 +23,9 @@ CREATE TABLE employee (
     first_name VARCHAR(45) NULL,
     last_name VARCHAR(45) NULL,
     role_id INT NULL,
-    manager_id INT NULL,
     INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    INDEX man_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE,
+	CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+	manager_id INT NULL,
+	INDEX man_ind (manager_id),
+	CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
