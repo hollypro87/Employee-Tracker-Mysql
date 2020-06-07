111d0cc74d3979c011ae7b921d07b43d1a7f46d3
Holly Prothe <hollyprothe1@gmail.com>
2020-06-07 14:58:25 -0500

    add functions to add and remove by different criteria and add find all by manager and by department

--- db/index.js
+++ db/index.js
@@ -25,5 +25,53 @@ class DB {
         return this.connection.query("DELETE FROM employee WHERE id = ?", employeeId);
     }
 
+    updateEmployeeRole(employeeId, roleId) {
+        return this.connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [roleId, employeeId]);
+    }
 
+    updateEmployeeManager(employeeId, managerId) {
+        return this.connection.query("UPDATE employee SET manager_id = ? WHERE id = ?", [managerId, employeeId]);
+    }
+
+    findAllRoles() {
+        return this.connection.query(
+            "SLECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    findAllDepartments() {
+        return this.connection.query(
+            "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+        );
+    }
+
+    createRole(role) {
+        return this.connection.query("INSERT INTO role SET ?", role);
+    }
+
+    removeRole(roleId) {
+        return this.connection.query("DELETE FROM role WHERE id = ?", roleId);
+    }
+
+    removeDepartment(departmentId) {
+        return this.connection.query("DELETE FROM department WHERE id = ?", departmentId);
+    }
+
+    createDeparment(departmentId) {
+        return this.connection.query("INSERT INTO department SET ?", department);
+    }
+
+    findAllByManager(managerId) {
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;", managerId
+        );
+    }
+
+    findAllByDepartment(departmentId) {
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;", departmentId
+        );
+    }
 }
+
+module.exports = new DB(connection);
